661ccfc1d28c68e28b90308dda6a864f84c602af
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-12-28 23:32:47 +0545

    Refactor CSS and HTML code for organization creation form

--- resources/views/organization/create.blade.php
+++ resources/views/organization/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,36 +8,38 @@
 
     <title>Organization Details Form</title>
 </head>
+
 <body>
 
-    <nav>
+    <nav class="nav">
         <h2>ACAS</h2>
     </nav>
 
-    <form action="{{route ('organization.store')}}" method="post">
+    <form action="{{ route('organization.store') }}" method="post" class="form">
         @csrf
         <h2>Organization Details</h2>
 
-        <label for="organizationName">Organization Name:</label>
-        <input type="text" id="organizationName" name="organizationName" required>
+        <label for="organizationName" class="label">Organization Name:</label>
+        <input type="text" id="organizationName" name="organizationName" class="input_label" required>
 
-        <label for="address">Address:</label>
-        <input type="text" id="address" name="organizationAddress" required>
+        <label for="address" class="label">Address:</label>
+        <input type="text" id="address" name="organizationAddress" class="input_label" required>
 
-        <label for="phoneNumber">Phone Number:</label>
-        <input type="number" id="phoneNumber" name="organizationPhoneNumber" required>
+        <label for="phoneNumber" class="label">Phone Number:</label>
+        <input type="number" id="phoneNumber" name="organizationPhoneNumber"class="input_label" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="organizationEmail" required>
+        <label for="email" class="label">Email:</label>
+        <input type="email" id="email" name="organizationEmail" class="input_label"required>
 
-        <label for="vatNo">VAT Number:</label>
-        <input type="number" id="vatNo" name="organizationVatNumber" required>
+        <label for="vatNo" class="label">VAT Number:</label>
+        <input type="number" id="vatNo" name="organizationVatNumber" class="input_label"required>
 
-        <label for="panNo">PAN Number:</label>
-        <input type="number" id="panNo" name="organizationPanNumber" required>
+        <label for="panNo" class="label">PAN Number:</label>
+        <input type="number" id="panNo" name="organizationPanNumber"class="input_label" required>
 
-        <button type="submit">Submit</button>
+        <button type="submit" class="submit_button">Submit</button>
     </form>
 
 </body>
+
 </html>
