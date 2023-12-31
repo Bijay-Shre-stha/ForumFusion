b14f74ac4de85166707a668c056e5f9b1ec3d982
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-12-31 20:39:32 +0545

    Refactor question index view

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -15,22 +15,23 @@
                 </div>
             </div>
             @foreach ($questions as $question)
-            <div class="card mb-3 mt-5 " style="max-width: 1000px;">
-               <div class="card p-3 fs-6 fw-bolder ">
-                Question:  {{$question->title}}
-                <br>
-                Description: {{$question->description}}
-            </div>
-                <div class="card-header">
-                    <a href="{{ route('question.show', $question->id) }}" class="btn btn-primary">Show</a>
-                    <a href="{{ route('question.edit', $question->id) }}" class="btn btn-success text-white ">Edit</a>
-                    <form action="{{ route('question.destroy', $question->id) }}" method="post" class="d-inline">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
+                <div class="card mb-3 mt-5 " style="max-width: 1000px;">
+                    <div class="card p-3 fs-6 fw-bolder ">
+                        Question: {{ $question->title }}
+                        <br>
+                        Description: {{ $question->description }}
+                    </div>
+                    <div class="card-header">
+                        <a href="{{ route('question.show', $question->id) }}" class="btn btn-primary">Show</a>
+                        <a href="{{ route('question.edit', $question->id) }}"
+                            class="btn btn-success text-white ">Edit</a>
+                        <form action="{{ route('question.destroy', $question->id) }}" method="post" class="d-inline">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
             @endforeach
         </div>
     </body>
