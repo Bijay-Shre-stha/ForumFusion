3669a2b0df92c0dde012c3f91b78180490559dd8
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-01-07 12:13:13 +0545

    Update navbar layout and add search  to forum index

--- resources/views/components/navbar-layout.blade.php
+++ resources/views/components/navbar-layout.blade.php
@@ -196,6 +196,6 @@
 <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script src="../assets/js/sidebarmenu.js"></script>
 <script src="../assets/js/app.min.js"></script>
-<script src="../assets/libs/simplebar/dist/simplebar.js"></script>
+{{-- <script src="../assets/libs/simplebar/dist/simplebar.js"></script> --}}
 
 </html>
--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -2,13 +2,28 @@
     @section('title', 'Forum')
 
     <body>
+        <!-- Search -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <form action="{{ route('forum.index') }}" method="get">
+                        <div class="input-group mb-3">
+                            <input type="text" name="search" class="form-control" placeholder="Search Question"
+                                aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- End Search -->
 
         @foreach ($questions as $question)
             <div class=" fw-bolder fs-6 mt-5 ">
-                {{ $loop->iteration }}. {{ucfirst( $question->title) }}
+                {{ $loop->iteration }}. {{ ucfirst($question->title) }}
                 <p class="mt-2">Description: {{ ucfirst($question->description) }}</p>
                 <p><small>Asked by :
-                        {{ $question->user->username }} <br>
+                        Unknown <br>
                         <b>-{{ $question->created_at->diffForHumans() }}</b>
                     </small></p>
             </div>
