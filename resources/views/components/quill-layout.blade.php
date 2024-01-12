a160992283a1c93fc1b13777e6bf060525121e13
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-01-12 07:32:11 +0545

    Update Quill layout in blade file

--- resources/views/components/quill-layout.blade.php
+++ resources/views/components/quill-layout.blade.php
@@ -2,28 +2,16 @@
 <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
 
 <!-- Create the editor container -->
-<div id="editor">
-    <p>Please Answer here...</p>
+<h3 class="mx-2">Your answer</h3>
+<div id="editor" class="editor">
 </div>
 
 <!-- Include the Quill library -->
 <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 
-<!-- Initialize Quill editor -->
+<!-- Initialize Quill editor and blur it -->
 <script>
     var quill = new Quill('#editor', {
         theme: 'snow'
-    });
-
-    var editorContainer = document.getElementById('editor');
-
-    document.addEventListener('click', function (event) {
-        if (!editorContainer.contains(event.target)) {
-            quill.root.innerHTML = '';
-        }
-        
-        if (!editorContainer.contains(event.target) && quill.container.contains(document.activeElement)) {
-            quill.root.innerHTML = '';
-        }
     });
 </script>
