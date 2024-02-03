fd368fa678773d071062fa1dc3c1f98b44a324a8
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-02-04 00:52:52 +0545

    refactored the community index

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -1,13 +1,28 @@
 <x-navbar-layout>
-    <a href="{{ route ('community.create')}}">
-        <button class="btn btn-success">Create community</button>
+
+    <h1 class="mt-4">Communities</h1>
+    <a href="{{ route('community.create') }}" class="m-4">
+        <button class="btn btn-success m-4">Create community</button>
     </a>
-    @foreach ($communities as $community)
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">{{ $community->communityName }}</h5>
-                {{-- <a href="{{ route('community.show', $community->id) }}" class="btn btn-primary">View</a> --}}
+
+    <div class="row">
+        @foreach ($communities as $community)
+            <div class="card">
+                <div class="card-body d-flex justify-content-between">
+                    <h5 class="">{{ $community->communityName }}</h5>
+                    <a href="{{ route('community.show', $community->communityName) }}" class="btn btn-primary">View</a>
+                </div>
+                <div class="card-footer d-flex justify-content-between m-3">
+                    <p class="text-muted cursor-pointer name_hover">
+                        Created by: <img src="{{ $community->user->avatar }}" alt="{{ $community->user->username }}"
+                            width="35" height="35" class="rounded-circle ">
+                        <span class="hover_show fw-bolder ">
+                            {{ $community->user->username }}
+                        </span>
+                    </p>
+                    <span class="fw-bolder">{{ \Carbon\Carbon::parse($community->created_at)->format('Y-m-d') }}</span>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </x-navbar-layout>
